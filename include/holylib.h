5af3e2b472e827dc6c601da282b5299313aa2cbd
YilloGMR <iwangpetradheerendra@gmail.com>
2026-03-14 17:23:25 +0700

    Prepare for merge with main

---
+++ include/holylib.h
@@ -0,0 +1,13 @@
+#ifndef HOLYLIB_H
+#define HOLYLIB_H
+
+#include <stdio.h>
+#include <string.h>
+
+// v1.0 (2026-03-11)
+
+void clean();
+void pause();
+int holyscanf(void *output, char type, int size);
+
+#endif
